df714cf71c0d48eb989a8e8f3a65b34960650aff
Trevor Scheer <trevor.scheer@gmail.com>
2026-09-25 15:14:44 +0000

    Refresh the Monaco bundle snapshot

--- packages/monaco-graphql/__tests__/monaco-editor.test.ts
+++ packages/monaco-graphql/__tests__/monaco-editor.test.ts
@@ -56,7 +56,6 @@ describe('monaco-editor', () => {
         "assets/graphql.js",
         "assets/graphqlMode.js",
         "assets/index.css",
-        "assets/jsonMode.css",
         "assets/jsonMode.js",
         "index.html",
         "index.js",
