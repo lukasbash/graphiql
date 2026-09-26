d0bbe1f14ea36e262a52c17bc4159386193e61f2
Trevor Scheer <trevor.scheer@gmail.com>
2026-09-26 14:08:01 +0000

    Load GraphQL bracket configuration eagerly

--- packages/monaco-graphql/src/monaco-editor.ts
+++ packages/monaco-graphql/src/monaco-editor.ts
@@ -1,5 +1,9 @@
 import 'monaco-editor/features/register.all';
 import 'monaco-editor/languages/definitions/graphql/register.js';
 import 'monaco-editor/languages/features/json/register.js';
+import { languages } from 'monaco-editor/editor';
+import { conf as graphqlLanguageConfiguration } from 'monaco-editor/languages/definitions/graphql/graphql.js';
+
+languages.setLanguageConfiguration('graphql', graphqlLanguageConfiguration);
 
 export * from 'monaco-editor/editor';
